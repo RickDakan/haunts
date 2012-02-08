5b8d71849bee7da56533a8e9625debc2eb06b8aa
Jonathan Wills <runningwild@gmail.com>
2012-02-07 17:13:27 -0800

    Forgot to add combat.go

---
+++ game/combat.go
@@ -0,0 +1,17 @@
+package game
+
+import (
+  "haunts/game/status"
+  "math/rand"
+)
+
+func DoAttack(attacker,defender *Entity, strength int, kind status.Kind) bool {
+  // get attacker's bonus for using the specified kind of attack
+  // get defender's bonus for defending against the specified kind of attack
+  // get the defender's current ego/corpus
+  // successful attack = strength + attack bonus + 1d10 >= defense bonus + ego/corpus
+  attack := attacker.Stats.AttackBonusWith(kind)
+  defense := defender.Stats.DefenseVs(kind)
+  roll := rand.Intn(10) + 1
+  return strength + attack + roll >= defense
+}
