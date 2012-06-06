baf1906b93bcc2aada6eecf4fb43f5f00e8c7e84
Jonathan Wills <runningwild@gmail.com>
2012-06-06 02:35:25 -0700

    Added some ai scripts I forgot about

---
+++ data/ais/corpse.lua
@@ -0,0 +1,43 @@
+intruders = nearestNEntities(3, "intruder")
+mypos = pos(me())
+
+intruder = intruders[1]
+if intruder then
+  if rangedDistBetween(me(), intruder) == 1 then
+    -- If we're right next to someone then we will either try to disease
+    -- them, if they aren't already diseased, otherwise we will just
+    -- attack them as much as possible
+    conditions = getConditions(intruder)
+    attack = ""
+    if conditions["Diseased Kiss"] then
+      attack = "Pummel"
+    else
+      attack = "Diseased Kiss"
+    end
+    while exists(intruder) do
+      res = doBasicAttack(attack, intruder)
+      if res.hit then
+        attack = "Pummel"
+      end
+    end
+  else
+    -- Path to a nearby intruder.  We'll pick the closest one if possible,
+    -- but if we can't get to that one for some reason then we'll pick a
+    -- different one.
+    for _, intruder in pairs(intruders) do
+      ps = allPathablePoints(mypos, pos(intruder), 1, 1)
+      if ps[1] then
+        doMove(ps, 1000)
+        break
+      end
+    end
+  end
+else
+  -- If there are no intruders in sight we walk around randomly
+  ps = allPathablePoints(mypos, mypos, 1, 3)
+  r = randN(table.getn(ps))
+  target = ps[randN(table.getn(ps))]
+  a = {}
+  a[1] = target
+  res = doMove(a, 1000)
+end
