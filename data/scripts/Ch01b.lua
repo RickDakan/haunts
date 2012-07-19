c6510c0b0051a6fb56b94e082ce734f2808e6418
Jonathan Wills <runningwild@gmail.com>
2012-07-18 23:43:55 -0700

    updated scripts

--- data/scripts/Ch01b.lua
+++ data/scripts/Ch01b.lua
@@ -1,35 +1,35 @@
-function setLosModeToRoomsWithSpawnsMatching(side, pattern)
-  sp = Script.GetSpawnPointsMatching(pattern)
-  rooms = {}
-  for i, spawn in pairs(sp) do
-    rooms[i] = Script.RoomAtPos(spawn.Pos)
-  end
-  Script.SetLosMode(side, rooms)
-end
+-- function setLosModeToRoomsWithSpawnsMatching(side, pattern)
+--   sp = Script.GetSpawnPointsMatching(pattern)
+--   rooms = {}
+--   for i, spawn in pairs(sp) do
+--     rooms[i] = Script.RoomAtPos(spawn.Pos)
+--   end
+--   Script.SetLosMode(side, rooms)
+-- end
 
-if not store.Ch01b then
-  store.Ch01b = {}
-  end
---
-play_as_denizens = false
+-- -- if not store.Ch01b then
+-- --   store.Ch01b = {}
+-- -- end
+-- --
+
 function Init()
-   store.Ch01b = {}
-   store.Ch01b.Spawnpoints_complete={}
-   store.Ch01b.Spawnpoints = {
-      "Ch01_Dialog01",
-      "Ch01_Dialog02",
-      "Ch01_Dialog03",
-      "Ch01_Dialog04",
-      "Ch01_Dialog05",
-      "Ch01_Dialog06",
-      "Ch01_Dialog07",
-      "Ch01_Dialog08",
-      "Ch01_Dialog09",
-      "Ch01_Dialog10",
-      "Shade Trigger01",
-      "Dining Trigger01",
-      "Harry Trigger01",
-   } 
+  store.Ch01b = {}
+  store.Ch01b.Spawnpoints_complete={}
+  store.Ch01b.Spawnpoints = {
+    "Ch01_Dialog01",
+    "Ch01_Dialog02",
+    "Ch01_Dialog03",
+    "Ch01_Dialog04",
+    "Ch01_Dialog05",
+    "Ch01_Dialog06",
+    "Ch01_Dialog07",
+    "Ch01_Dialog08",
+    "Ch01_Dialog09",
+    "Ch01_Dialog10",
+    "Shade Trigger01",
+    "Dining Trigger01",
+    "Harry Trigger01",
+  } 
 
 
   Script.LoadHouse("Chapter_01_b")
@@ -39,18 +39,17 @@ function Init()
   Script.BindAi("intruder", "human")
  
   intruder_spawn = Script.GetSpawnPointsMatching("Intruders-Start")
+  print("Found", table.getn(intruder_spawn), "points")
   Script.SpawnEntitySomewhereInSpawnPoints("Caitlin", intruder_spawn)
   Script.SpawnEntitySomewhereInSpawnPoints("Percy", intruder_spawn)
-  ents = Script.GetAllEnts()
-  end
 end
  
 
 function RoundStart(intruders, round)
-    Script.SetVisibility("intruders")
-    Script.SetLosMode("intruders", "entities")
-    Script.SetLosMode("denizens", "entities")
-    Script.ShowMainBar(intruders ~= play_as_denizens)
+  Script.SetVisibility("intruders")
+  Script.SetLosMode("intruders", "entities")
+  Script.SetLosMode("denizens", "entities")
+  Script.ShowMainBar(intruders)
 end
 
 
@@ -59,9 +58,7 @@ function pointIsInSpawn(pos, sp)
 end
 
 function pointIsInSpawns(pos, regexp)
-  print("Regexp: '",regexp,"'")
   sps = Script.GetSpawnPointsMatching(regexp)
-  print("Got ", "spawns for ", regexp)
   for _, sp in pairs(sps) do
     if pointIsInSpawn(pos, sp) then
       return true
@@ -71,11 +68,11 @@ function pointIsInSpawns(pos, regexp)
 end
 
 
-function IsPosInUnusedSpawnpoint(pos, name, list)
+function IsPosInUnusedSpawnpoint(pos, list, used)
   --name identifies spawnpoint
   for _, spawn in pairs(list) do
-    if not used[name] and pointIsInSpawns(pos, name) then
-      return name
+    if not used[spawn] and pointIsInSpawns(pos, spawn) then
+      return spawn
     end
   end
   return nil
@@ -86,7 +83,9 @@ function OnMove(ent, path)
     return table.getn(path)
   end
   for i, pos in pairs(path) do
-    name = IsPosInUnusedSpawnpoint(pos, store.Spawnpoints, store.Spawnpoints_complete)
+    print("path", pos.X, pos.Y)
+    name = IsPosInUnusedSpawnpoint(pos, store.Ch01b.Spawnpoints, store.Ch01b.Spawnpoints_complete)
+    print("Name: ", name)
     if name then
       return i
     end
@@ -95,73 +94,105 @@ function OnMove(ent, path)
 end
 
 function OnAction(intruders, round, exec)
-  if not exec.ent.Side.Intruder then
+  if not exec.Ent.Side.Intruder then
     return
   end
-  name = IsPosInUnusedSpawnpoint(pos, store.Spawnpoints, store.Spawnpoints_complete)
-  if name then
-    dialog_path = "ui/dialog/Ch01/" .. name .. ".json"
-    Script.DialogBox(dialog_path)
-    store.Ch01.Spawnpoints_complete[name] = true 
-    if name == "Ch01_Dialog02" then
-      Script.LoadScript("Chapter_01_b")
-    end
-
-    if name == "Shade Trigger01" then
-      shade_spawn = Script.GetSpawnPointsMatching("Shade Spawn01")
-      for i = 1 do
-        ent = SpawnEntitySomewhereInSpawnPoints("Shade", shade_spawn)
-        store.temporary_shades[i] = ent
-    end
-
-    if name == "Dining Trigger01" then
-      shade_spawn = Script.GetSpawnPointsMatching("Shade Spawn02")
-
-      store.temporary_shades = {}
-      for i = 1,4 do
-        ent = SpawnEntitySomewhereInSpawnPoints("Shade", shade_spawn)
-        store.temporary_shades[i] = ent
-      end
-    end
-
-    if name == "Harry Trigger01" then
-      Script.DialogBox(ui/dialog/Ch01/"Ch01_Dialog04")
-      harry_spawn = Script.GetSpawnPointsMatching("Harry Spawn")
-
-      store.angry_shades = {}
-      for i = 1,5 do
-        ent = SpawnEntitySomewhereInSpawnPoints("Angry Shade", angry_shade_spawn)
-        SpawnEntitySomewhereInSpawnPoints("Scared Man", harry_spawn)
-     end  
+  name = IsPosInUnusedSpawnpoint(exec.Ent.Pos, store.Ch01b.Spawnpoints, store.Ch01b.Spawnpoints_complete)
+  if name == "Ch01_Dialog03" then
+    Script.DialogBox("ui/dialog/Ch01/Ch01_Dialog03.json")
+    store.Ch01b.Dialog_complete["Ch01_Dialog03"] = true
   end
-
-  all_dead = true
-  for _, ent in pairs(store.angry_shades) do
-    if ent.Stats.HpCur > 0 then
-      all_dead = false
-    end
+  if name == "Ch01_Dialog04" then
+    Script.DialogBox("ui/dialog/Ch01/Ch01_Dialog04.json")
+    store.Ch01b.Dialog_complete["Ch01_Dialog04"] = true
   end
-  if all_dead then
+  if name == "Ch01_Dialog05" then
+    Script.DialogBox("ui/dialog/Ch01/Ch01_Dialog05.json")
+    store.Ch01b.Dialog_complete["Ch01_Dialog05"] = true
+  end
+  if name == "Ch01_Dialog06" then
     Script.DialogBox("ui/dialog/Ch01/Ch01_Dialog06.json")
-    Script.LoadScript("Chapter_01_c")
+    store.Ch01b.Dialog_complete["Ch01_Dialog06"] = true
   end
+  if name == "Ch01_Dialog07" then
+    Script.DialogBox("ui/dialog/Ch01/Ch01_Dialog07.json")
+    store.Ch01b.Dialog_complete["Ch01_Dialog07"] = true
+  end
+  if name == "Ch01_Dialog08" then
+    Script.DialogBox("ui/dialog/Ch01/Ch01_Dialog08.json")
+    store.Ch01b.Dialog_complete["Ch01_Dialog08"] = true
+  end
+  if name == "Ch01_Dialog09" then
+    Script.DialogBox("ui/dialog/Ch01/Ch01_Dialog09.json")
+    store.Ch01b.Dialog_complete["Ch01_Dialog09"] = true
+  end
+  if name == "Ch01_Dialog10" then
+    Script.DialogBox("ui/dialog/Ch01/Ch01_Dialog10.json")
+    store.Ch01b.Dialog_complete["Ch01_Dialog10"] = true
+  end
+
+  if name == "Shade Trigger01" then
+    print("SPAWN SHADE")
+    shade_spawn = Script.GetSpawnPointsMatching("^Shade Spawn01$")
+    print("found", shade_spawn)
+    ent = Script.SpawnEntitySomewhereInSpawnPoints("Shade", shade_spawn)
+    print("ENT:", ent.Name)
+    store.Ch01b.Spawnpoints_complete["Shade Trigger01"] = true
+  end
+
+--     if name == "Dining Trigger01" then
+--       shade_spawn = Script.GetSpawnPointsMatching("Shade Spawn02")
+
+--       store.temporary_shades = {}
+--       for i = 1,4 do
+--         ent = SpawnEntitySomewhereInSpawnPoints("Shade", shade_spawn)
+--         store.temporary_shades[i] = ent
+--       end
+--     end
+
+--     if name == "Harry Trigger01" then
+--       Script.DialogBox(ui/dialog/Ch01/"Ch01_Dialog04")
+--       harry_spawn = Script.GetSpawnPointsMatching("Harry Spawn")
+
+--       store.angry_shades = {}
+--       for i = 1,5 do
+--         ent = SpawnEntitySomewhereInSpawnPoints("Angry Shade", angry_shade_spawn)
+--         SpawnEntitySomewhereInSpawnPoints("Scared Man", harry_spawn)
+--      end  
+--   end
+
+--   all_dead = true
+--   for _, ent in pairs(store.angry_shades) do
+--     if ent.Stats.HpCur > 0 then
+--       all_dead = false
+--     end
+--   end
+--   if all_dead then
+--     Script.DialogBox("ui/dialog/Ch01/Ch01_Dialog06.json")
+--     Script.LoadScript("Chapter_01_c")
+--   end
+-- end
+
+-- --ANY WAY TO CHANGE THE TIMING ON THIS?
+-- --if ent.HpCur > 1 then 
+-- --  Script.SetHp(ent, 1)
+-- --if ent.HpCur == 1 then
+-- --  Script.SetHp(ent, 0)
+-- --end
 end
 
---ANY WAY TO CHANGE THE TIMING ON THIS?
---if ent.HpCur > 1 then 
---  Script.SetHp(ent, 1)
---if ent.HpCur == 1 then
---  Script.SetHp(ent, 0)
---end
-
 function RoundEnd(intruders, round)
-  for _, ent in pairs(store.temporary_shades) do
-    if ent.HpCur > 1 then 
-      Script.SetHp(ent, 1)
-    if ent.HpCur == 1 then
-      Script.SetHp(ent, 0)
+  if not intruders then
+    for _, ent in pairs(Script.GetAllEnts()) do
+      if ent.Name == "Shade" then
+        if ent.HpCur == 1 then
+          Script.PlayAnimations(ent, {"defend", "killed"})
+          Script.SetHp(ent, 0)
+        end
+        if ent.HpCur > 1 then 
+          Script.SetHp(ent, 1)
+        end
+      end
     end
   end
-  store.temporary_shades = nil
-  print("end", intruders, round)
 end
--- data/scripts/foo.lua
+++ data/scripts/foo.lua
@@ -7,11 +7,15 @@ function setLosModeToRoomsWithSpawnsMatching(side, pattern)
   Script.SetLosMode(side, rooms)
 end
 
+if not store.Ch01a then
+  store.Ch01a = {}
+  end
+--
 play_as_denizens = false
 function Init()
-   store.Ch01 = {}
-   store.Ch01.Dialog_complete = {}
-   store.Ch01.Dialogs = {
+   store.Ch01a = {}
+   store.Ch01a.Spawnpoints_complete={}
+   store.Ch01a.Spawnpoints = {
       "Ch01_Dialog01",
       "Ch01_Dialog02",
       "Ch01_Dialog03",
@@ -24,174 +28,25 @@ function Init()
       "Ch01_Dialog10",
    } 
 
-while false do
-    choices = Script.DialogBox("ui/dialog/sample.json")
-    print("Choices made:")
-    for _, choice in pairs(choices) do
-      print("Chose: ", choice)
-    end
-  end
+  Script.LoadHouse("Chapter_01_a")
+  -- Script.DialogBox("ui/dialog/Ch01/Ch01_Dialog01.json") 
 
-
-  Script.LoadHouse("dialog_test")
-   
-  sp = Script.GetSpawnPointsMatching("Foo-.*")
-
-  if play_as_denizens then
-    Script.BindAi("denizen", "human")
-    Script.BindAi("minions", "minions.lua")
-    Script.BindAi("intruder", "intruders.lua")
-  else
-    Script.BindAi("denizen", "denizens.lua")
-    Script.BindAi("minions", "minions.lua")
-    Script.BindAi("intruder", "human")
-  end
-end
-
-function doDenizenSetup()
-  -- This creates a list of entities and associated point values.  The order
-  -- the names are listed in here is the order they will appear to the user.
-  ents = {
-    {"Chosen One", 1},
-    {"Master of the Manse", 1},
-  }
-
-  setLosModeToRoomsWithSpawnsMatching("denizens", "Master-.*")
-
-  -- Now we give the user a ui with which to place these entities.  The user
-  -- will have 1 point to spend, and each of the options costs one point, so
-  -- they will only place 1.  We will make sure they place exactly one.
-  -- Also the "Master-.*" indicates that the entity can only be placed in
-  -- spawn points that have a name that matches the regular expression
-  -- "Master-.*", which means anything that begins with "Master-".  So
-  -- "Master-BackRoom" and "Master-Center" both match, for example.
-  placed = {}
-  while table.getn(placed) == 0 do
-    placed = Script.PlaceEntities("Master-.*", 1, ents)
-    
-  end
-
-  -- placed is an array containing all of the entities placed, in this case
-  -- there will only be one, and we will use that one to determine what
-  -- servitors to make available to the user to place.
-  if placed[1].Name == "Chosen One" then
-    ents = {
-      {"Disciple", 1},
-      {"Devotee", 1},
-      {"Eidolon", 3},
-    }
-  else
-    ents = {
-      {"Corpse", 1},
-      {"Vengeful Wraith", 1},
-      {"Poltergeist", 1},
-      {"Angry Shade", 1},
-    }
-  end
-
-  -- Just like before the user gets a ui to place these entities, but this
-  -- time they can place more, and this time they go into spawn points that
-  -- match anything with the prefix "Servitor-".
-  setLosModeToRoomsWithSpawnsMatching("denizens", "Servitor-.*")
-  placed = Script.PlaceEntities("Servitor-.*", 10, ents)
-
-  -- set the denizens to not be able to see anything - it's not very
-  -- significant since their turn is about to end anyway.
-  Script.SetLosMode("denizens", "none")
-end
-
-function doIntrudersSetup()
-  -- Let the intruders choose from among one of the three game modes.
-  -- Currently the data isn't stored or used anywhere, but this gives you an
-  -- idea of how a menu can be created.
-  -- modes = {}
-  -- modes["Cleanse"] = "ui/explorer_setup/cleanse.png"
-  -- modes["Relic"] = "ui/explorer_setup/relic.png"
-  -- modes["Mystery"] = "ui/explorer_setup/mystery.png"
-  -- print("Last time picked: ",   store.mode)
-  -- r = Script.PickFromN(1, 1, modes)
-  -- for i,name in pairs(r) do
-  --   print("This time picked: ", i, name)
-  -- end
-  -- store.mode = r[1]
-  -- Script.SaveStore()
-  -- -- Find the "Intruders-FrontDoor" spawn point and spawn a Teen, Occultist,
-  -- -- and Ghost Hunter there.  Additionally we will mind the
-  -- -- sample_aoe_occultist.lua ai to the occultist.
+  Script.BindAi("denizen", "denizens.lua")
+  Script.BindAi("minions", "minions.lua")
+  Script.BindAi("intruder", "human")
+    --always bind one to human!
   intruder_spawn = Script.GetSpawnPointsMatching("Intruders-FrontDoor")
-  -- print("intruder spawn:", intruder_spawn)
-  -- for k,v in pairs(intruder_spawn) do
-  --   print("kv: ", k, v)
-  -- end
-  Script.SpawnEntitySomewhereInSpawnPoints("Teen", intruder_spawn)
-  ent = Script.SpawnEntitySomewhereInSpawnPoints("Occultist", intruder_spawn)
-  Script.BindAi(ent, "sample_aoe_occultist.lua")
-  Script.SpawnEntitySomewhereInSpawnPoints("Ghost Hunter", intruder_spawn)
+  Script.SpawnEntitySomewhereInSpawnPoints("Caitlin", intruder_spawn)
+  Script.SpawnEntitySomewhereInSpawnPoints("Percy", intruder_spawn)
   ents = Script.GetAllEnts()
-
-  -- This lets you pick gear for each entity, you can uncomment this block
-  -- if you want to turn it on.
-  -- for en, ent in pairs(ents) do
-  --   print("Checking ent: ", ent.Name)
-  --   for i, gear in pairs(ent.GearOptions) do
-  --     print("gear", en, i, gear)
-  --   end
-  --   count = 0
-  --   for _, _ in pairs(ent.GearOptions) do
-  --     count = count + 1
-  --   end
-  --   if count > 0 then
-  --     r = Script.PickFromN(1, 1, ent.GearOptions)
-  --     for i,name in pairs(r) do
-  --       print("picked", i, name)
-  --     end
-  --     Script.SetGear(ent, r[1])
-  --   end
-  -- end
 end
+ 
 
 function RoundStart(intruders, round)
-  if round == 1 then
-    if intruders then
-      Script.SetVisibility("intruders")
-      doIntrudersSetup()
-
-      relic_spawn = Script.GetSpawnPointsMatching("Relic-.*")
-      Script.SpawnEntitySomewhereInSpawnPoints("Scepter", relic_spawn)
-      Script.SpawnEntitySomewhereInSpawnPoints("Tome", relic_spawn)
-    else
-      -- Script.SetVisibility("denizens")
-      -- doDenizenSetup()
-    end
-    -- Make it clear that the players don't get to activate their units on the
-    -- setup turn.
-    Script.EndPlayerInteraction()
-    return
-  end
-
-  -- The start of round 2 is when a player's visibility should be only what
-  -- their units are able to see.  It is important to call setVisibility
-  -- because it sets what the player can see, and it is important to call
-  -- setLosMode because it sets what is technically visible to entities on
-  -- each side.
-  if round == 2 then
-    if play_as_denizens then
-      Script.SetVisibility("denizens")
-    else
-      Script.SetVisibility("intruders")
-    end
+    Script.SetVisibility("intruders")
     Script.SetLosMode("intruders", "entities")
     Script.SetLosMode("denizens", "entities")
-  end
-  Script.ShowMainBar(intruders ~= play_as_denizens)
-
-  -- This is sample code to spawn one angry shade at the start of each
-  -- denizens' turn.
-  -- if not intruders then
-  --   spawn_points = Script.GetSpawnPointsMatching("Minion-.*")
-  --   p = Script.SpawnEntitySomewhereInSpawnPoints("Angry Shade", spawn_points)
-  --   print("Spawned Angry Shade at ", p.x, p.y)
-  -- end
+    Script.ShowMainBar(intruders ~= play_as_denizens)
 end
 
 
@@ -200,9 +55,7 @@ function pointIsInSpawn(pos, sp)
 end
 
 function pointIsInSpawns(pos, regexp)
-  print("Regexp: '",regexp,"'")
   sps = Script.GetSpawnPointsMatching(regexp)
-  print("Got ", "spawns for ", regexp)
   for _, sp in pairs(sps) do
     if pointIsInSpawn(pos, sp) then
       return true
@@ -211,56 +64,49 @@ function pointIsInSpawns(pos, regexp)
   return false
 end
 
-function OnMove(ent, path)
-  -- DIALOG
-  -- We want to check if an intruder is about to walk through one of the
-  -- waypoints we've set up.  Ideally we'd check first that ent.Side.Intruder
-  -- is true, but that isn't working in this build (I've fixed it already though).
-  -- path is an array of the points that the entity is about to walk through, so
-  -- we check them in order to see if any overlap the next waypoint that should
-  -- trigger.  If it does then we set the corresponding value in Dialog_complete
-  -- to "DOIT", which is our signal to pop up the dialog box when the action
-  -- completes.
-  for _, dialog in pairs(store.Ch01.Dialogs) do
-    -- We don't want to truncate movement for dialog that has already happened,
-    -- so we check Dialog_complete before anything else.
-    if not store.Ch01.Dialog_complete[dialog] then
-      for i, pos in pairs(path) do
-        if pointIsInSpawns(pos, dialog) then
-          -- If we need to trigger the dialog then we set the appropriate value
-          -- to "DOIT" and return the distance along this path that the ent
-          -- should move.
-          store.Ch01.Dialog_complete[dialog] = "DOIT"
-          return i
-        end
-      end
+function IsPosInUnusedSpawnpoint(pos, list, used)
+  --name identifies spawnpoint
+  for _, spawn in pairs(list) do
+    if not used[spawn] and pointIsInSpawns(pos, spawn) then
+      return spawn
     end
   end
+  return nil
+end
 
-  -- If we made it here then there was no dialog that needed to be shown, so
-  -- we don't want to truncate the movement action, so we return the length
-  -- of the path.
+
+--THIS STOPS a unit in a spawn point not yet activated.
+function OnMove(ent, path)
+  if not ent.Side.Intruder then
+    return table.getn(path)
+  end
+  for i, pos in pairs(path) do
+    name = IsPosInUnusedSpawnpoint(pos, store.Ch01a.Spawnpoints, store.Ch01a.Spawnpoints_complete)
+    if name then
+      return i
+    end
+  end
   return table.getn(path)
 end
 
 function OnAction(intruders, round, exec)
-  -- DIALOG
-  -- Check for that value "DOIT", if we find it then we pop up the dialog box.
-  for _, dialog in pairs(store.Ch01.Dialogs) do
-   --  if not store.Ch01.Dialog_complete[dialog] then
-   --   return
-   -- end
-    if store.Ch01.Dialog_complete[dialog] == "DOIT" then
-      -- Note that the .. operator is string concatenation in lua.
-      dialog_path = "ui/dialog/Ch01/" .. dialog .. ".json"
-      Script.DialogBox(dialog_path)  -- pop up the dialog box
-      store.Ch01.Dialog_complete[dialog] = true
-      -- keep track so we don't do it again later
-      return
-    end
+  if not exec.Ent.Side.Intruder then
+    return
+  end
+  name = IsPosInUnusedSpawnpoint(exec.Ent.Pos, store.Ch01a.Spawnpoints, store.Ch01a.Spawnpoints_complete)
+  if name then
+    dialog_path = "ui/dialog/Ch01/" .. name .. ".json"
+    Script.DialogBox(dialog_path)
+    store.Ch01a.Spawnpoints_complete[name] = true 
+    if name == "Ch01_Dialog02" then
+      Script.StartScript("Ch01b.lua")
+    --INSERT other names and functions here
+    end  
   end
 end
+ 
 
 function RoundEnd(intruders, round)
   print("end", intruders, round)
 end
+
