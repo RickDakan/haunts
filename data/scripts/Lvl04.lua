927d4885792ddf181005983b6dc6df9b87956e23
Rick <rick@mobrulesgames.com>
2012-09-10 08:34:26 -0400

    level 4 and 5 scripts, newestest

--- data/scripts/Lvl04.lua
+++ data/scripts/Lvl04.lua
@@ -39,22 +39,18 @@ function Init(data)
     Script.BindAi("intruder", "human")
   end
 
-<<<<<<< HEAD
-=======
   --spawn an initial beacon
-  print("SCRIPT: ", "Beacon")
   Script.SpawnEntitySomewhereInSpawnPoints("Beacon", Script.GetSpawnPointsMatching("Beacon_Start"), false)
 
->>>>>>> Jonathan/devel
   --set these modular variables.
   store.MasterName = nil
   store.IntrudersPlacedBeaconLastTurn = false
   store.BeaconEnt = nil
   store.BeaconCount = 0
+  store.nBeaconedRooms = 0
 end
 
 function intrudersSetup()
-
   if IsStoryMode() then
     intruder_names = {"Nikola Fury", "Peter Chelios", "Tracy Latona"}
     intruder_spawn = Script.GetSpawnPointsMatching("Intruders_Start")
@@ -63,18 +59,10 @@ function intrudersSetup()
   end 
 
   for _, name in pairs(intruder_names) do
-<<<<<<< HEAD
-    ent = Script.SpawnEntitySomewhereInSpawnPoints(name, intruder_spawn)
-=======
-    print("SCRIPT:", name)
     ent = Script.SpawnEntitySomewhereInSpawnPoints(name, intruder_spawn, false)
->>>>>>> Jonathan/devel
     Script.SetCondition(ent, "Pitch Black", true)
     Script.SetGear(ent, "Beacons")
   end
-
-  
-  --tore.BeaconEnt = ent
 
   -- Choose entry point here.
   Script.SaveStore()
@@ -129,14 +117,16 @@ function denizensSetup()
 end
 
 function RoundStart(intruders, round)
-  if store.execs == nil then
+
+  if not store.execs then
     store.execs = {}
   end
+
   if round == 1 then
     if intruders then
       intrudersSetup()     
     else
-      -- Script.DialogBox("ui/dialog/Lvl04/Lvl_04_Opening_Denizens.json")
+      Script.DialogBox("ui/dialog/Lvl04/Lvl_04_Opening_Denizens.json")
       denizensSetup()
     end
     Script.SetLosMode("intruders", "blind")
@@ -148,12 +138,14 @@ function RoundStart(intruders, round)
 
     Script.EndPlayerInteraction()
 
-    print("SCRIPT: End round 1")
     return
   end
 
-  if store.IntrudersPlacedBeaconLastTurn then
+  if intruders and round > 1 then
     SetActivatedRooms()
+  end
+
+  if store.IntrudersPlacedBeaconLastTurn then
     Script.SetVisibility("denizens")
     setLosModeToRoomsWithSpawnsMatching("denizens", "Servitors_Start")
     placed = Script.PlaceEntities("Servitors_Start", ServitorEnts, 0, ValueForReinforce())
@@ -239,6 +231,9 @@ end
 
 function OnAction(intruders, round, exec)
   -- Check for players being dead here
+  if store.execs == nil then
+    store.execs = {}
+  end
   store.execs[table.getn(store.execs) + 1] = exec
 
   if exec.Action.Name == "Place Beacon" then
@@ -260,6 +255,12 @@ function OnAction(intruders, round, exec)
   --The big check: Have the intruders won.
   if exec.Action.Name == "Place Beacon" then
     SetActivatedRooms()
+
+    if (store.Room1 or store.Room2 or store.Room3 or store.Room4 or store.Room5) and not store.bTalkedAboutBeaconInFirstRoom then
+      store.bTalkedAboutBeaconInFirstRoom = true
+      Script.DialogBox("ui/dialog/Lvl04/Lvl_04_First_Beacon_Intruders.json")
+    end    
+
     if store.Room1 and store.Room2 and store.Room3 and store.Room4 and store.Room5 then
       --Intruders win
       Script.DialogBox("ui/dialog/Lvl04/Lvl_04_Victory_Intruders.json")
@@ -273,6 +274,10 @@ function OnAction(intruders, round, exec)
 
   --If the vampire attacks, he automatically moves back to his lair.
   if exec.Ent.Name == "Vampire" and (exec.Action.Type == "Basic Attack" or exec.Action.Type == "Aoe Attack") then
+    if not store.bToldDenisAboutVampireTeleport then
+      store.bToldDenisAboutVampireTeleport = true
+      Script.DialogBox("ui/dialog/Lvl04/Lvl_04_Vampire_Teleport_Denizens.json")
+    end
     StoreTeleport(exec.Ent, Script.GetSpawnPointsMatching("Master_Start")[1].Pos)
     doTeleport(teleport_exec)
     StoreCondition("Illuminated", exec.Ent, false)
@@ -296,12 +301,12 @@ function OnAction(intruders, round, exec)
   end   
 
   --after any action, if this ent's Ap is 0, we can select the next ent for them
-  -- if exec.Ent.ApCur == 0 then
-  --   nextEnt = GetEntityWithMostAP(exec.Ent.Side)
-  --   if nextEnt.ApCur > 0 then
-  --     Script.SelectEnt(nextEnt)
-  --   end
-  -- end
+  if exec.Ent.ApCur == 0 then
+    nextEnt = GetEntityWithMostAP(exec.Ent.Side)
+    if nextEnt.ApCur > 0 then
+      Script.SelectEnt(nextEnt)
+    end
+  end
 end
 
 function SetActivatedRooms()
@@ -310,26 +315,44 @@ function SetActivatedRooms()
   store.Room3 = false
   store.Room4 = false
   store.Room5 = false
+
+  StoreWaypoint("Room1", "intruders", (Script.GetSpawnPointsMatching("Room1_mid")[1].Pos), 5, false)
+  StoreWaypoint("Room2", "intruders", (Script.GetSpawnPointsMatching("Room2_mid")[1].Pos), 5, false)
+  StoreWaypoint("Room3", "intruders", (Script.GetSpawnPointsMatching("Room3_mid")[1].Pos), 5, false)
+  StoreWaypoint("Room4", "intruders", (Script.GetSpawnPointsMatching("Room4_mid")[1].Pos), 5, false)
+  StoreWaypoint("Room5", "intruders", (Script.GetSpawnPointsMatching("Room5_mid")[1].Pos), 5, false)
+
   store.BeaconCount = 0
+  store.nBeaconedRooms = 0  --need 2 counters b/c they can put > 1 beacon per room
 
   for _, beaconEnt in pairs(Script.GetAllEnts()) do
     if beaconEnt.Name == "Beacon" then
       --Bacon ent!!!
       store.BeaconCount = store.BeaconCount + 1
-      if pointIsInSpawns(beaconEnt.Pos, "Room1") then
+      if pointIsInSpawns(beaconEnt.Pos, "Room1") and not store.Room1 then
         store.Room1 = true
+        StoreWaypoint("Room1", "", "", "", true)
+        store.nBeaconedRooms = store.nBeaconedRooms + 1
       end
-      if pointIsInSpawns(beaconEnt.Pos, "Room2") then
+      if pointIsInSpawns(beaconEnt.Pos, "Room2") and not store.Room2 then
         store.Room2 = true
+        StoreWaypoint("Room2", "", "", "", true)
+        store.nBeaconedRooms = store.nBeaconedRooms + 1
       end      
-      if pointIsInSpawns(beaconEnt.Pos, "Room3") then
+      if pointIsInSpawns(beaconEnt.Pos, "Room3") and not store.Room3 then
         store.Room3 = true
+        StoreWaypoint("Room3", "", "", "", true)
+        store.nBeaconedRooms = store.nBeaconedRooms + 1
       end
-      if pointIsInSpawns(beaconEnt.Pos, "Room4") then
+      if pointIsInSpawns(beaconEnt.Pos, "Room4") and not store.Room4 then
         store.Room4 = true
+        StoreWaypoint("Room4", "", "", "", true)
+        store.nBeaconedRooms = store.nBeaconedRooms + 1
       end
-      if pointIsInSpawns(beaconEnt.Pos, "Room5") then
+      if pointIsInSpawns(beaconEnt.Pos, "Room5") and not store.Room5 then
         store.Room5 = true
+        StoreWaypoint("Room5", "", "", "", true)
+        store.nBeaconedRooms = store.nBeaconedRooms + 1
       end   
     end
   end
@@ -369,11 +392,22 @@ function RoundEnd(intruders, round)
     end
 
     if intruders then
-      Script.DialogBox("ui/dialog/Lvl04/pass_to_denizens.json")
+      Script.DialogBox("ui/dialog/Lvl04/pass_to_denizens.json", {rooms=(5-store.nBeaconedRooms)})
+
+      if store.bTalkedAboutBeaconInFirstRoom and not store.bToldDenisAboutFirstBeacon then
+        store.bToldDenisAboutFirstBeacon = true
+        Script.DialogBox("ui/dialog/Lvl04/Lvl_04_First_Beacon_Denizens.json")
+      end
 
     else
       store.IntrudersPlacedBeaconLastTurn = false
-      Script.DialogBox("ui/dialog/Lvl04/pass_to_intruders.json")
+      Script.DialogBox("ui/dialog/Lvl04/pass_to_intruders.json", {rooms=(5-store.nBeaconedRooms)})
+
+      if store.bToldDenisAboutVampireTeleport and not store.bToldIntrudersAboutVampireTeleport then
+        store.bToldIntrudersAboutVampireTeleport = true
+        Script.DialogBox("ui/dialog/Lvl04/Lvl_04_Vampire_Teleport_Intruders.json")
+      end
+
       if not store.bDoneIntruderIntro then
         store.bDoneIntruderIntro = true
         Script.DialogBox("ui/dialog/Lvl04/Lvl_04_Opening_Intruders.json")
@@ -384,16 +418,17 @@ function RoundEnd(intruders, round)
     Script.SetLosMode("intruders", "entities")
     Script.SetLosMode("denizens", "entities")
     Script.LoadGameState(store.game)
+
+    --focus the camera on somebody on each team.
+    side2 = {Intruder = not intruders, Denizen = intruders, Npc = false, Object = false}  --reversed because it's still one side's turn when we're replaying their actions for the other side.
+    Script.FocusPos(GetEntityWithMostAP(side2).Pos)
+
     for _, exec in pairs(store.execs) do
       bDone = false
       if exec.script_spawn then
         doSpawn(exec)
         bDone = true
       end
-      -- if exec.script_despawn then
-      --   deSpawn(exec)
-      --   bDone = true
-      -- end
       if exec.script_gear then
         doGear(exec)
         bDone = true
@@ -406,6 +441,10 @@ function RoundEnd(intruders, round)
         doTeleport(exec)
         bDone = true
       end     
+      if exec.script_waypoint then
+        doWaypoint(exec)
+        bDone = true
+      end         
       if not bDone then
         Script.DoExec(exec)
 
@@ -475,19 +514,28 @@ function deSpawn(despawnExec)
 end
 
 function GetDistanceBetweenEnts(ent1, ent2)
-  return (math.abs(ent1.Pos.X - ent2.Pos.X) + math.abs(ent1.Pos.Y - ent2.Pos.Y))
+  v1 = ent1.Pos.X - ent2.Pos.X
+  if v1 < 0 then
+    v1 = 0-v1
+  end
+  v2 = ent1.Pos.Y - ent2.Pos.Y
+  if v2 < 0 then
+    v2 = 0-v2
+  end
+  return v1 + v2
 end
 
 function GetDistanceBetweenPoints(pos1, pos2)
-  return (math.abs(pos1.X - pos2.X) + math.abs(pos1.Y - pos2.Y))
+  v1 = pos1.X - pos2.X
+  if v1 < 0 then
+    v1 = 0-v1
+  end
+  v2 = pos1.Y - pos2.Y
+  if v2 < 0 then
+    v2 = 0-v2
+  end
+  return v1 + v2
 end
-
--- function IntruderWithBeaconsAlive()
---   if store.BeaconEnt.HpCur > 0 then
---     return true
---   end
---   return false  
--- end
 
 function GetSpawnsFromListWhereNoLoS(spawns)
   -- for _, possibleSpawn in pairs(spawns) do
@@ -545,17 +593,11 @@ function AnyIntrudersAlive()
   end
   return false  
 end
-<<<<<<< HEAD
-=======
 
 function StoreWaypoint(wpname, wpside, wppos, wpradius, wpremove)
-  print("SCRIPT: 1")
   waypoint_exec = {script_waypoint=true, name=wpname, side=wpside, pos=wppos, radius=wpradius, remove=wpremove}
-  print("SCRIPT: 2")
   store.execs[table.getn(store.execs) + 1] = waypoint_exec
-  print("SCRIPT: 3")
   doWaypoint(waypoint_exec)
-  print("SCRIPT: 4")
 end
 
 function doWaypoint(waypointExec)
@@ -565,4 +607,3 @@ function doWaypoint(waypointExec)
     return Script.SetWaypoint(waypointExec.name, waypointExec.side, waypointExec.pos, waypointExec.radius)
   end
 end
->>>>>>> Jonathan/devel
